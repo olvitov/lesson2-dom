afee651ef3a529dde60bad944bc6bedce2d2c072
olvit <olvitov@gmail.com>
2016-05-17 13:18:23 +0300

    Соеденение с БД

---
+++ www/classes/DB.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: olvit
+ * Date: 17.05.2016
+ * Time: 11:33
+ */
+class DB
+{
+    public function __construct()
+    {
+      mysql_connect('localhost', 'root', '');
+        mysql_select_db('test');
+    }
+
+    public function query($sql, $class = 'stdClass') {
+
+        $res = mysql_query($sql);
+        if (false === $res) {
+            return false;
+
+        }
+        $ret = [];
+        while ($row = mysql_fetch_object($res, $class)) {
+            $ret[] = $row;
+        }
+        return $ret;
+    }
+
+}
