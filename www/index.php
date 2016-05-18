fb2c0bdd8ac211d15bdf6e13ae6f3babc338b245
olvit <olvitov@gmail.com>
2016-05-18 13:38:49 +0300

    Индекс пхп

--- www/index.php
+++ www/index.php
@@ -1,9 +1,12 @@
 <?php
 
-require __DIR__ . '/models/News.php';
+ require_once __DIR__ . '/autoload.php';
 
-$items = News::getAll();
+$ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
-// var_dump($items);
-
-include __DIR__ . '/views/index.php';
+$controllerClassName = $ctrl . 'Controller';
+// require_once __DIR__ . '/controllers/' . $controllerClassName . '.php';
+$controller = new $controllerClassName;
+$method = 'action' . $act;
+$controller->$method();
