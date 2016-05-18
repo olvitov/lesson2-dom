cdfc0c1ce8d49584e9df927c6a6f94792b5de837
olvit <olvitov@gmail.com>
2016-05-18 13:50:43 +0300

    Автолоад добавлены классы

--- www/autoload.php
+++ www/autoload.php
@@ -6,6 +6,8 @@ function __autoload($class) {
         require __DIR__ .'/controllers/' . $class . '.php';
     } elseif ((file_exists(__DIR__ . '/models/' . $class . '.php'))) {
         require __DIR__ .'/models/' . $class . '.php';
+    } elseif ((file_exists(__DIR__ . '/classes/' . $class . '.php'))) {
+        require __DIR__ . '/models/' . $class . '.php';
     }
     
 }
