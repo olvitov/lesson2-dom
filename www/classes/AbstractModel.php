74a76d02e9bafbbea2ed68cab3f2a10f49e8eb35
olvit <olvitov@gmail.com>
2016-05-26 14:31:06 +0300

    ORM

--- www/classes/AbstractModel.php
+++ www/classes/AbstractModel.php
@@ -9,4 +9,24 @@ abstract class AbstractModel
     {
         return static::$table;
     }
+
+    public static function findAll() {
+
+        $class = get_called_class();
+        $sql = 'SELECT * FROM ' . static::$table;
+        $db = new DB();
+        $db->setClassName($class);
+        return $db->query($sql);
+    }
+public static function findOneByPk($id) {
+
+    $class = get_called_class();
+
+    $sql = 'SELECT * FROM ' .static::$table . ' WHERE id=:id';
+    $db = new DB();
+    $db->setClassName($class);
+    return $db->query($sql, [':id' => $id]);
+
+}
+
 }
