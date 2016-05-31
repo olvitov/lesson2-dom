a2d488223f314ed6bf3275a6cd256279aa809ef4
olvit <olvitov@gmail.com>
2016-05-31 17:07:34 +0300

    Orm update save

---
+++ www/models/NewsModel.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Class NewsModel
+ * @property $id
+ * @property $title
+ * @property $text
+ * @property $author
+ * 
+ */
+
+class NewsModel
+extends AbstractModel
+{
+
+    protected static $table = 'news';
+
+    
+ /*   
+    
+   public $id;
+  public $title;
+    public $text;
+   public $author;
+*/
+
+}
